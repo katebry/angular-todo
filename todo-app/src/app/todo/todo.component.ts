872346252c97cdc48c1cc51db9d724f9f382624e
Kate Bryan <katebryan31@gmail.com>
2021-03-13 15:59:57 +0000

    feat: added Todo interface

--- todo-app/src/app/todo/todo.component.ts
+++ todo-app/src/app/todo/todo.component.ts
@@ -1,18 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from '../todo';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.sass']
+  styleUrls: ['./todo.component.sass'],
 })
 export class TodoComponent implements OnInit {
-  
-  constructor() { }
-  
-  todo = 'Go to the bank'
+  todo: Todo = {
+    id: 1,
+    task: 'Go to the bank',
+  };
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
-
+  ngOnInit(): void {}
 }
