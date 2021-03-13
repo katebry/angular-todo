47e986ff90a0aee5fc99522ff556123f8b2bbaa2
Kate Bryan <katebryan31@gmail.com>
2021-03-13 15:23:27 +0000

    chore: updated test suites / title

--- todo-app/src/app/app.component.spec.ts
+++ todo-app/src/app/app.component.spec.ts
@@ -23,13 +23,13 @@ describe('AppComponent', () => {
   it(`should have as title 'todo-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('todo-app');
+    expect(app.title).toEqual('Angular To-do App');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('todo-app app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Angular To-do App is running!');
   });
 });
