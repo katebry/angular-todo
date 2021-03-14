648125cd96e6acc66ea6ca90ff1c31462d0d8f01
Kate Bryan <katebryan31@gmail.com>
2021-03-14 07:52:15 +0000

    feat: added mock http server error handling

--- todo-app/src/app/todo.service.ts
+++ todo-app/src/app/todo.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 import { Todo } from './todo';
@@ -9,18 +10,36 @@ import { TODOS } from './mock-todos';
   providedIn: 'root',
 })
 export class TodoService {
-  constructor(private messageService: MessageService) {}
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+  ) {}
+
+  private todosUrl = 'api/todos';
+
+  private log(message: string) {
+    this.messageService.add(`TodoService: ${message}`);
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error); 
+      this.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    };
+  }
 
   getTodos(): Observable<Todo[]> {
-    const todos = of(TODOS);
-    this.messageService.add('TodoService: fetched todos');
-    return todos;
+    return this.http.get<Todo[]>(this.todosUrl).pipe(tap(_ => this.log('fetched todos')),
+      catchError(this.handleError<Todo[]>('getTodos', []))
+    );
   }
 
   getTodo(id: number): Observable<Todo> {
-    // TODO: send the message _after_ fetching the hero
-    this.messageService.add(`TodoService: fetched todo id=${id}`);
-    return of(TODOS.find(todo => todo.id === id));
+    const url = `${this.todosUrl}/${id}`;
+    return this.http.get<Todo>(url).pipe(
+      tap(_ => this.log(`fetched todo id=${id}`)),
+      catchError(this.handleError<Todo>(`getTodo id=${id}`))
+    );
   }
-
 }
